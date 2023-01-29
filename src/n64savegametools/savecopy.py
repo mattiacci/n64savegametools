0a7fe34dd779da752316b13cf9d5e704b093d8f9
Michael Mattiacci <mattiacci@gmail.com>
2023-01-28 18:06:55 -0800

    Clean up in prep for adding Mupen64Plus format

--- src/n64savegametools/savecopy.py
+++ src/n64savegametools/savecopy.py
@@ -7,15 +7,14 @@ from enum import Enum
 import logging
 from pathlib import Path
 import shutil
-from typing import Dict,List,NamedTuple,Optional,Union
-from n64savegametools.savegames import Everdrive64Savegames,Project64Savegames
+from typing import Union
+from n64savegametools.savegames import Everdrive64Savegames,Project64Savegames,Savegames
 
 logger = logging.getLogger(__name__)
 
 class SaveFormat(str, Enum):
     ED64 = "ED64"
     PJ64 = "PJ64"
-    RETROARCH = "RETROARCH"
 
 ### config that should be externalized
 
@@ -40,37 +39,23 @@ def copy_saves_for_rom(rom_path: Union[Path, str], src_save_format: SaveFormat, 
         raise FileNotFoundError("Provided source save path isn't a directory: {}".format(src_savedir))
     if not dst_savedir.is_dir():
         raise FileNotFoundError("Provided destination save path isn't a directory: {}".format(dst_savedir))
+    if src_savedir.samefile(dst_savedir):
+        raise FileExistsError("Provided source save path and destination save path are the same: {}".format(src_savedir))
     logger.info("ROM: %s", rom_path)
     logger.info("source: %s, %s", src_save_format, src_savedir)
     logger.info("destination: %s, %s", dst_save_format, dst_savedir)
-
-    src_savegames = Project64Savegames() if src_save_format == SaveFormat.PJ64 else Everdrive64Savegames()
+    src_savegames = _create_new_savegames(src_save_format)
     src_savegames.import_from_disk(rom_path, src_savedir)
-    dst_savegames = Project64Savegames() if dst_save_format == SaveFormat.PJ64 else Everdrive64Savegames()
+    dst_savegames = _create_new_savegames(dst_save_format)
     dst_savegames.import_from_savegames(src_savegames)
     # TODO: Should this method do a backup first, or should that be a separate call?
     dst_savegames.export_to_disk(rom_path, dst_savedir)
     logger.info("copy complete: %s", rom_path)
 
-# class OptionalSaveFiles(NamedTuple):
-#     eeprom: Optional[Path] = None
-#     sram: Optional[Path] = None
-#     flashram: Optional[Path] = None
-#     mpks: List[Optional[Path]] = [None, None, None, None]
-
-# def _backup_save_files(save_files: Optional[OptionalSaveFiles]):
-#     if save_files:
-#         for key in save_files._fields:
-#             src_file = getattr(save_files, key)
-#             if isinstance(src_file, list):
-#                 for item in src_file:
-#                     if item:
-#                         _replace_file(item, item.parent / "backup" / item.name)
-#             elif src_file:
-#                 _replace_file(src_file, src_file.parent / "backup" / src_file.name)
-
-# def _replace_file(src_path: Optional[Path], dst_path: Optional[Path]):
-#     if src_path is not None and dst_path is not None:
-#         logger.debug("rename from %s to %s", src_path, dst_path)
-#         dst_path.parent.mkdir(parents = True, exist_ok=True)
-#         src_path.replace(dst_path)
+def _create_new_savegames(format: SaveFormat) -> Savegames:
+    if format == SaveFormat.ED64:
+        return Everdrive64Savegames()
+    elif format == SaveFormat.PJ64:
+        return Project64Savegames()
+    else:
+        raise NotImplementedError()
--- src/n64savegametools/savegames.py
+++ src/n64savegametools/savegames.py
@@ -23,12 +23,6 @@ class SavegameData(NamedTuple):
     data: bytes
     endianness: Endianness = Endianness.BIG_ENDIAN
 
-class SaveFiles(NamedTuple):
-    eeprom: Path
-    sram: Path
-    flashram: Path
-    mpks: List[Path]
-
 class Savegames():
     eeprom: Optional[SavegameData] = None
     sram: Optional[SavegameData] = None
@@ -61,13 +55,13 @@ class Everdrive64Savegames(Savegames):
         if files.flashram.is_file():
             self.flashram = SavegameData(files.flashram.read_bytes())
         self.mpks = [SavegameData(mpk.read_bytes()) if mpk.is_file() else None for mpk in files.mpks]
-    def _get_save_files(self, rom_path: Path, savedir_path: Path) -> SaveFiles:
+    def _get_save_files(self, rom_path: Path, savedir_path: Path) -> _MultipleSaveFiles:
         if not rom_path.is_file():
             raise FileNotFoundError("Provided ROM path doesn't exist")
         if not savedir_path.is_dir():
             raise FileNotFoundError("Provided save directory doesn't exist")
         game_filename_stem = rom_path.stem
-        return SaveFiles(
+        return _MultipleSaveFiles(
             eeprom=savedir_path / "{}.eep".format(game_filename_stem),
             sram=savedir_path / "{}.srm".format(game_filename_stem),
             flashram=savedir_path / "{}.fla".format(game_filename_stem),
@@ -91,7 +85,7 @@ class Project64Savegames(Savegames):
         if files.flashram.is_file():
             self.flashram = SavegameData(_pad(files.flashram.read_bytes(), _expected_bytesizes["flashram"]), Endianness.LITTLE_ENDIAN)
         self.mpks = [SavegameData(mpk.read_bytes()) if mpk.is_file() else None for mpk in files.mpks]
-    def _get_save_files(self, rom_path: Path, savedir_path: Path) -> SaveFiles:
+    def _get_save_files(self, rom_path: Path, savedir_path: Path) -> _MultipleSaveFiles:
         if not rom_path.is_file():
             raise FileNotFoundError("Provided ROM path doesn't exist")
         if not savedir_path.is_dir():
@@ -101,7 +95,7 @@ class Project64Savegames(Savegames):
             raise IOError("Could not get info from ROM: {}".format(rom_path))
         savedir_path = savedir_path / "{}-{}".format(rom_info.internal_name, rom_info.hash_little_endian_md5.upper())
         game_filename_stem = rom_info.internal_name
-        return SaveFiles(
+        return _MultipleSaveFiles(
             eeprom=savedir_path / "{}.eep".format(game_filename_stem),
             sram=savedir_path / "{}.sra".format(game_filename_stem),
             flashram=savedir_path / "{}.fla".format(game_filename_stem),
@@ -112,6 +106,12 @@ _expected_bytesizes: Dict[str, Union[int, List[int]]] = {
     "eeprom": [512, 2048],
     "flashram": 131072,
 }
+
+class _MultipleSaveFiles(NamedTuple):
+    eeprom: Path
+    sram: Path
+    flashram: Path
+    mpks: List[Path]
 
 def _export_to_file(savegame_data: Optional[SavegameData], dst_path: Path, endianness = Endianness.BIG_ENDIAN):
     if savegame_data is not None and dst_path is not None:
